3800e20d30d5a3dec502c87a1d65c8247d9c868b
Felipe Endlich <endlichfelipe@gmail.com>
2023-01-28 18:07:05 -0300

    database middleware

--- src/main.py
+++ src/main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
+
+from .core.middleware.database_session_middleware import DatabaseSessionMiddleware
 from .core.router import router
 
 def build_api() -> FastAPI:
     application = FastAPI()
+
+    application.add_middleware(DatabaseSessionMiddleware)
 
     application.include_router(router, prefix="/api")
 
