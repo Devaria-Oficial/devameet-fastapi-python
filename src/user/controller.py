0e461723a32357d938418a5933cb765847fcddc7
Felipe Endlich <endlichfelipe@gmail.com>
2023-01-28 23:18:31 -0300

    user method

--- src/user/controller.py
+++ src/user/controller.py
@@ -1,7 +1,8 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Body, Depends
 
 from src.auth.handler import get_current_user
 
+from .schema import UpdateUser
 from .service import UserService
 
 
@@ -10,3 +11,11 @@ router = APIRouter()
 @router.get('/')
 async def read_users_me(username: str = Depends(get_current_user), service: UserService = Depends(UserService)):
     return service.get_user_by_username(username)
+
+@router.put('/{id}')
+async def update_user(
+    id: str, 
+    user_data: UpdateUser = Body(embed=False), 
+    username: str = Depends(get_current_user), 
+    service: UserService = Depends(UserService)):
+    return service.update_user(id, username, user_data)
--- src/user/service.py
+++ src/user/service.py
@@ -1,6 +1,9 @@
 from fastapi import Depends
+from src.core.middleware.error import ApiError
 from src.auth.model import User
 from src.core.database import SessionLocal, get_db
+
+from .schema import UpdateUser
 
 
 class UserService:
@@ -9,3 +12,15 @@ class UserService:
 
     def get_user_by_username(self, username: str):
         return self.db.query(User).filter(User.username == username).first()
+
+    def update_user(self, user_id : str, username: str, update_user_dto: UpdateUser):
+        user = self.db.query(User).filter(User.id == user_id, User.username == username).first()
+        if not user:
+            raise ApiError(message="Cannot update this user", error="Bad Request", status_code=400)
+
+        user.name = update_user_dto.name
+        user.avatar = update_user_dto.avatar
+        self.db.commit()
+        self.db.refresh(user)
+
+        return user
