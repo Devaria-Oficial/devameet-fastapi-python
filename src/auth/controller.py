0515b39334fa2aa2b1b51403fa0ec335bb30bbcb
Felipe Endlich <endlichfelipe@gmail.com>
2023-01-28 22:22:04 -0300

    generate jwt token on login

--- src/auth/controller.py
+++ src/auth/controller.py
@@ -9,12 +9,10 @@ from .schema import Login, Register
 router = APIRouter()
 
 @router.post('/login')
-async def login(user: Login):
-    service = AuthService()
-    return service.login(user)
+async def login(login_dto: Login, service: AuthService = Depends(AuthService)):
+    return service.login(login_dto)
 
 
 @router.post('/register')
-async def register(user: Register, db: Session = Depends(get_db)):
-    service = AuthService(db)
-    return service.register(user)
+async def register(register_dto: Register, service: AuthService = Depends(AuthService)):
+    return service.register(register_dto)
--- src/auth/service.py
+++ src/auth/service.py
@@ -1,22 +1,29 @@
-from fastapi import HTTPException
+from fastapi import Depends, HTTPException
 from passlib.context import CryptContext
 
 from sqlalchemy.orm import Session
 
-from .schema import Login, Register
+from src.core.database import get_db
+
+from .handler import TokenHandler
+from .schema import Login, Register, Token
 from .model import User
 
 
 class AuthService:
-    def __init__(self, db: Session):
+    def __init__(self, db: Session = Depends(get_db)):
         self.pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
         self.db = db
 
-    def login(self, user: Login):
-        if not (user.login == 'teste@teste.com' and user.password == '123456'):
-            raise HTTPException(400, 'Invalid login or password')
-        
-        return {'message': 'Logged in'}
+    def login(self, login_dto: Login):
+        user = self.db.query(User).filter(User.username == login_dto.login).first()
+        if not user:
+            raise Exception("Incorrect username or password") # ApiError(message="Incorrect username or password", error="Incorrect username or password", status_code=401)
+        if not self.pwd_context.verify(login_dto.password, user.hashed_password):
+            raise Exception("Incorrect username or password") # ApiError(message="Incorrect username or password", error="Incorrect username or password", status_code=401)
+
+        token = TokenHandler.create_access_token(data={"sub": user.username})
+        return Token(token=token, name=user.name, email=user.username)
 
     def register(self, register_dto: Register):
         hashed_password = self.pwd_context.hash(secret=register_dto.password)
