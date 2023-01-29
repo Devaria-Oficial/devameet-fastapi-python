eafdbb33d2f42f759a8f7c2997ed3b3a98f3dec5
Felipe Endlich <endlichfelipe@gmail.com>
2023-01-29 09:38:19 -0300

    position model

--- migrations/env.py
+++ migrations/env.py
@@ -13,6 +13,7 @@ if config.config_file_name is not None:
 from src.meet.model import Meet
 from src.meet.model import ObjectMeet
 from src.auth.model import User
+from src.room.model import Position
 from src.core.database import Base
 
 target_metadata = Base.metadata
