b021cce019e7c1bc00fc81b8650c81b4dc4a86a9
Felipe Endlich <endlichfelipe@gmail.com>
2023-01-28 22:16:15 -0300

    token schema

--- src/auth/schema.py
+++ src/auth/schema.py
@@ -10,3 +10,8 @@ class Register(BaseModel):
     password: str = Field(..., min_length=8, max_length=20, regex='((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$')
     name: str = Field(..., min_length=2)
     avatar: str
+
+class Token(BaseModel):
+    email: str
+    name: str
+    token: str
